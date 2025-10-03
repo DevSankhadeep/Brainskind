94f62cafda1395ef9f81b0d6cc1362f492a6e6fc
solanki30 <dassolanki182@gmail.com>
2025-10-03 19:23:24 +0530

    add

--- BACKEND/middleware/isAuthenticated.js
+++ BACKEND/middleware/isAuthenticated.js
@@ -8,10 +8,9 @@ const authenticateToken = (req, res, next) => {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         if(!decoded) {
             return res.status(401).json({message:"invalid token",success: false,});
-
-            req.userId = decoded.userId;
-            next();
         }
+        req.userId = decoded.userId;
+        next();
         
     } catch (error) {
         return res.status(500).json({message:"Invalid token",success: false,});
